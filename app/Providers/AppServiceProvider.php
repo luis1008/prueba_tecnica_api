6259b38550a33a54bff681beead32e0bf794c998
Jorge Luis Gutierrez Martinez <jorge.gutierrez@bodesa.mx>
2025-09-19 11:15:53 -0600

    Se modificó el archivo provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Jenssegers\Mongodb\Schema\Blueprint;
+use Jenssegers\Mongodb\Schema\Builder;
+use Illuminate\Database\Schema\Blueprint as OriginalBlueprint;
+use Illuminate\Database\Schema\Builder as OriginalBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->app->bind(OriginalBlueprint::class, Blueprint::class);
+        $this->app->bind(OriginalBuilder::class, Builder::class);
     }
 }
